94e18939a6d3c488ce6d8eaaf6a3e910c2c3bab4
John Chritton <jchritton33@gmail.com>
2018-02-23 04:08:10 -0600

    Package update

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const sassMiddleware = require('node-sass-middleware');
+
 const env = require('../config/env');
 
 mongoose.Promise = global.Promise;
@@ -10,37 +12,50 @@ const index = require('./routes');
 
 const app = express();
 
-//Port setup
+// Port setup
 const port = process.env.PORT || 8080;
 
-//Set up path for views and view engine
+// Set up path for views and view engine
 app.set('views', path.join(__dirname, '../client/views'));
 app.set('view engine', 'pug');
 
-//Set up directory for static served files
+// Compile Sass to CSS
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, '../client/sass'),
+    dest: path.join(__dirname, '../../public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    sourceMapEmbed: true,
+    prefix: '/css'
+  })
+);
+
+// Set up directory for static served files
 app.use(express.static(path.join(__dirname, '../../public')));
 
-//Set up index as router on root
+// Set up index as router on root
 app.use('/', index);
 
-//Pass error to error handler for rendering 404.
+// Pass error to error handler for rendering 404.
 app.use((req, res, next) => {
   let error = new Error('Not found');
   error.status = 404;
   next(error);
 });
 
-//Error handler
+// Error handler
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  //Render error page based on passed status
+  // Render error page based on passed status
   res.status(err.status || 500);
   res.render('error404');
 });
 
-//Start server
+// Start server
 app.listen(port, () => {
   console.log('Image search server is listening on port ' + port);
 });
